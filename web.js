8fa5b444f7671c624d5e019b97509e848b407a03
hyesung5 <hgang865@gmail.com>
2021-11-22 10:50:44 +0900

    1050

--- web.js
+++ web.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const PORT = 8001
-app.set('views', __dirname + '/views');
-app.set('view engine', 'html')
-app.get('/', (req, res) => {
-  res.render('index')
-})
-app.listen(PORT, () => {
-    console.log(`server started on PORT ${PORT}`)
-})
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8001;
+app.use(express.static("uploads"));
+
+app.use(express.static(path.join(__dirname, "/views/build")));
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname, "/views/build/index.html"));
+});
+
+app.get("*", function (req, res) {
+	res.sendFile(path.join(__dirname, "/views/build/index.html"));
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
